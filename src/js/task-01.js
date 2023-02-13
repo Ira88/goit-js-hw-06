25c5b6fd9c25e7fb5cad8ff967753a4baab392c0
Ira88 <111458593+Ira88@users.noreply.github.com>
2023-02-13 15:12:51 +0200

    task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,6 @@
-console.log("hello");
+let categories = document.querySelectorAll('.item');
+console.log('Number of categories: ${categories.length} ');
+categories.forEach(item => {
+    console.log('Category: ${item.firstElementChild.textContent}');
+    console.log('Elements: ${item.lastElementChild.children.length}')
+})
