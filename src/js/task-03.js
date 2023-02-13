d424e00d05534310b6de9ecd3dc7d4424fc2b947
Ira88 <111458593+Ira88@users.noreply.github.com>
2023-02-13 18:25:52 +0200

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -16,18 +16,17 @@ const list = document.querySelector(".gallery");
 const htmlMarkup = images
 .map(
   (image) => 
-  `<li class="photo"><img src="${image.url}" width="333" hight="200" alt="${image.alt}"></li>`
+  `<li><img class="photo" src="${image.url}" width="400" alt="${image.alt}"></li>`
 ).join("");
 list.insertAdjacentHTML("beforeend", htmlMarkup);
 
 const photos = document.querySelectorAll(".photo");
 list.style.display = "flex";
-list.style.gap = "30px";
+list.style.gap = "50px";
 list.style.padding = "50px";
 list.style.justifyContent = "centre";
 list.style.alingnItems = "center";
 list.style.listStyleType = "none";
-list.style.borderRadius = "50px";
 list.style.backgroundColor = "darkseagreen";
 
 photos.forEach(photo => photo.style.borderRadius = "8px");
