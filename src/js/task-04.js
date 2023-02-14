3c505230e8f0ba538cfa8de639afecf334857d83
Ira88 <111458593+Ira88@users.noreply.github.com>
2023-02-14 13:50:28 +0200

    task5

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,12 +1,13 @@
-let counterValue = 0;
-let counter = document.querySelector('#value');
-const minusBtn = document.querySelector('button[data-action="decrement"]');
-const plusBtn = document.querySelector('button[data-action="increment"]');
-minusBtn.addEventListener('clik', () => {
-    counterValue -= 1;
-    counter.textContent = counterValue;
-});
-plusBtn.addEventListener('clik', () => {
-    counterValue += 1;
-    counter.textContent = counterValue;
-})
+ let counterValue = 0;
+ let counter = document.querySelector('#value');
+ const minusBtn = document.querySelector('button[data-action="decrement"]');
+ const plusBtn = document.querySelector('button[data-action="increment"]');
+ minusBtn.addEventListener('clik', () => {
+     counterValue -= 1;
+     counter.textContent = counterValue;
+ });
+ plusBtn.addEventListener('clik', () => {
+     counterValue += 1;
+     counter.textContent = counterValue;
+ })
+
