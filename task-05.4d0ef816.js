70aced55ec7e6f34df2b7ee49cd768546fd98307
Ira88 <111458593+Ira88@users.noreply.github.com>
2023-02-14 11:50:54 +0000

    Deploying to gh-pages from @ Ira88/goit-js-hw-06@3c505230e8f0ba538cfa8de639afecf334857d83 🚀

---
+++ task-05.4d0ef816.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("#name-input"),e=document.querySelector("#name-output");t.addEventListener("input",(t=>{e.textContent=t.target.value,""===t.target.value&&(e.textContent="Anonymous")}));
+//# sourceMappingURL=task-05.4d0ef816.js.map
---
+++ task-05.ee1dc721.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("#name-input"),e=document.querySelector("#name-output");t.addEventListener("input",(function(t){e.textContent=t.target.value,""===t.target.value&&(e.textContent="Anonymous")}))}();
+//# sourceMappingURL=task-05.ee1dc721.js.map
