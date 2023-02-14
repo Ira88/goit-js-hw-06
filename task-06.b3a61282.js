df14385a9e72dc0a030f88513c14c1a4efeb5abf
Ira88 <111458593+Ira88@users.noreply.github.com>
2023-02-14 12:15:15 +0000

    Deploying to gh-pages from @ Ira88/goit-js-hw-06@5997e180c8407e44607a952315dbe20c13d51e56 🚀

---
+++ task-06.b3a61282.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("#validation-input");t.addEventListener("blur",(function(a){a.target.value.length==t.qetAttribute("data-length")?(t.classList.add("valid"),t.classList.contains("invalid")&&t.classList.remove("invalid")):(t.classList.contains("valid")&&t.classList.remove("valid"),t.classList.add("invalid"))}))}();
+//# sourceMappingURL=task-06.b3a61282.js.map
